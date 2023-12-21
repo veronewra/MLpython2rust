a91e795de2b13bf15c5382a22e72aff7754dc2fd
Veronika <vero.newra@gmail.com>
2023-12-20 18:18:55 -0800

    bandaided the y part of python data

--- bayes example/linfa_bayes/target/debug/linfa_bayes.d
+++ bayes example/linfa_bayes/target/debug/linfa_bayes.d
@@ -1,1 +1,1 @@
-/home/veronika/MLrust/linfa_bayes/target/debug/linfa_bayes: /home/veronika/MLrust/linfa_bayes/src/main.rs
+/home/veronika/MLrust/MLpython2rust/bayes\ example/linfa_bayes/target/debug/linfa_bayes: /home/veronika/MLrust/MLpython2rust/bayes\ example/linfa_bayes/src/main.rs
